67f9533e825d021f9fa6448872034f06c4dc2579
Diego Muñoz Diaz <dmunozdiaz@gmail.com>
2016-08-05 08:39:03 -0300

    Se agregan cambios a la interfaz de búsqueda de usuarios con boostrap

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,15 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10
   def index
+    @page = (params[:page || 0]).to_i
   	if params[:keywords].present?
   		@keywords = params[:keywords]
   		customer_serach_term = CustomerSearchTerm.new(@keywords)
   		@customers = Customer.where(customer_serach_term.where_clause,
   			customer_serach_term.where_args	 
-  			).order(customer_serach_term.order)
+  			)
+        .order(customer_serach_term.order)
+        .offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
   	else
   		@customers = []
   	end	
