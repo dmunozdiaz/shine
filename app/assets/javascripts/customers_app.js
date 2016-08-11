bbef8777b4de846dd9df79f7f0b1af912b1729f6
Diego Muñoz Diaz <dmunozdiaz@gmail.com>
2016-08-11 08:55:51 -0300

    Se agrega manejo de rutas con angular y rails

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -14,14 +14,17 @@ app.config([
             $routeProvider.when("/",{
                 controller:     "CustomerSearchController",
                 templateUrl:    "customer_search.html"
+            }).when("/:id",{
+                controller: "CustomerDetailController",
+                templateUrl: "customer_detail.html"
             });
         }
     ]);
 
 
 app.controller("CustomerSearchController", [
-    "$scope", "$http",
-    function($scope, $http) {
+    "$scope", "$http","$location",
+    function($scope, $http, $location) {
         var page = 0;
         $scope.customers = [];
         $scope.search = function(searchTerm) {
@@ -49,5 +52,27 @@ app.controller("CustomerSearchController", [
         	page = page + 1;
         	$scope.search($scope.keywords);
         }
+
+        $scope.viewDetails = function(customer){
+            $location.path("/"+customer.id);
+        }
     }
 ]);
+
+app.controller("CustomerDetailController", [
+    "$scope", "$http", "$routeParams",
+    function($scope, $http, $routeParams){
+        var customerId = $routeParams.id;
+        $scope.customer = {};
+
+        $http.get("/customers/"+customerId+".json")
+            .then(
+                function(response){
+                    $scope.customer = response.data;
+                },function(response){
+                    alert("There was a problem: "+ response.status);
+                }
+            );
+
+    }
+    ]);
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
-  resources   :customers, only: [:index]
+  resources   :customers, only: [:index, :show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
